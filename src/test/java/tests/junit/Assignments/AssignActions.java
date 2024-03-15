7a658616c23235f15d26f6c369f5307996f00145
Areej <areej262626@gmail.com>
2024-03-15 18:11:05 +0300

    Assignments

--- src/test/java/tests/junit/Assignments/AssignActions.java
+++ src/test/java/tests/junit/Assignments/AssignActions.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import tests.junit.Tests;
 
@@ -12,15 +13,18 @@ public class AssignActions extends Tests {
     /** go to https://www.google.com/
      * search for "Scroll Methods" by using an Actions object
      */
-    Actions actions = new Actions(driver);
+
 
 @Test
 public void assign4(){
     //go to https://www.google.com/
+    driver = new ChromeDriver();
+    Actions actions = new Actions(driver);
 driver.navigate().to("https://www.google.com/");
     WebElement searchInput = driver.findElement(By.id("APjFqb"));
     //search for "Scroll Methods" by using an Actions object
-    actions.sendKeys(searchInput, "Scroll Methods").sendKeys(Keys.ENTER);
+    searchInput.sendKeys("Scroll Methods");
+    searchInput.submit();
 
 }
 }
--- src/test/java/tests/junit/Assignments/AssignColor.java
+++ src/test/java/tests/junit/Assignments/AssignColor.java
@@ -1,6 +1,7 @@
 package tests.junit.Assignments;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -12,7 +13,8 @@ import tests.junit.Tests;
 import java.time.Duration;
 
 public class AssignColor extends Tests {
-    /** Go to URL: https://api.jquery.com/dblclick/
+    /**
+     * Go to URL: https://api.jquery.com/dblclick/
      * Double click on the blue square at the bottom of the page and then write the changed color.
      * Go top of the page and sendKeys "Thanks to JavascriptExecutor"
      * Go to the bottom of the page and click the book, name"jQuery in Action"
@@ -20,35 +22,38 @@ public class AssignColor extends Tests {
 
 
     @Test
-    public void dbClick(){
+    public void dbClick() {
+        WebDriver driver = new ChromeDriver();
         Actions actions = new Actions(driver);
-        //Go to URL: https://api.jquery.com/dblclick/
-        driver.navigate().to("https://api.jquery.com/dblclick/");
-
-
-
-        WebElement dbclickElement = driver.findElement(By.tagName("iframe"));
-        driver.switchTo().frame(dbclickElement);
-        actions.doubleClick(dbclickElement).perform();
-        //Double click on the blue square at the bottom of the page and then write the changed color.
-        WebElement blueColour = driver.findElement(By.tagName("div"));
-
-        actions.doubleClick(blueColour).perform();
-        String changedColor = blueColour.getCssValue("background-color");
-        System.out.println("Changed color: " + changedColor);
-
-        // Go top of the page and sendKeys "Thanks to JavascriptExecutor"
         JavascriptExecutor js = (JavascriptExecutor) driver;
-        WebElement body = driver.findElement(By.tagName("body"));
-       js.executeScript("arguments[0].innerText = 'Thanks to JavascriptExecutor'", body);
 
-        //Go to the bottom of the page and click the book, name"jQuery in Action"
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(100));
-        WebElement book = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='https://www.manning.com/books/jquery-in-action-third-edition']")));
-        book.click();
+        try {
+            // Go to URL: https://api.jquery.com/dblclick/
+            driver.get("https://api.jquery.com/dblclick/");
 
+            // Double click on the blue square at the bottom of the page and then write the changed color.
+            WebElement blueSquare = driver.findElement(By.cssSelector("body>div"));
+            actions.doubleClick(blueSquare).perform();
+            String changedColor = blueSquare.getCssValue("background-color");
+            System.out.println("Changed color: " + changedColor);
 
-
+            // Go to the top of the page and sendKeys "Thanks to JavascriptExecutor"
+            js.executeScript("window.scrollTo(0, 0)");
+            WebElement body = driver.findElement(By.tagName("body"));
+            js.executeScript("arguments[0].innerText = 'Thanks to JavascriptExecutor'", body);
+            // Go to the bottom of the page and click the book named "jQuery in Action"
+            WebElement book = driver.findElement(By.xpath("//a[@href='https://www.manning.com/books/jquery-in-action-third-edition']"));
+            js.executeScript("arguments[0].scrollIntoView();", book);
+            book.click();
+            // Verify the current URL after clicking the book link
+            String currentURL = driver.getCurrentUrl();
+            Assertions.assertEquals("https://www.manning.com/books/jquery-in-action-third-edition", currentURL);
+        } catch (NoSuchElementException e) {
+            // Handle the NoSuchElementException and print an error message
+            System.err.println("Element not found: " + e.getMessage());
+        } finally {
+            // Quit the WebDriver instance to close the browser
+            driver.quit();
+        }
     }
-    }
-
+}
